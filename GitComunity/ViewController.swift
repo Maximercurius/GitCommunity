2943fdfbc6cdd4e0844ca07b76b5b4f07fab3d9b
Maxim <swiftdevfromsiberia@gmail.com>
2022-10-01 22:43:04 +0500

    Initial Commit

---
+++ GitComunity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 01.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
