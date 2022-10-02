5dd0ec3d802748ed4f562305cbaefcf5feeb53fb
Maxim <swiftdevfromsiberia@gmail.com>
2022-10-02 14:40:05 +0500

    Home UITabBar conroller created. Search and Navigation VCs are created

---
+++ GitComunity/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 02.10.2022.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+    }
+
+}
---
+++ GitComunity/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 02.10.2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
