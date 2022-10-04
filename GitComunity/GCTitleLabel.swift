c3250a54969b11cad16fbd75c8c8bb2db142a150
Maxim <swiftdevfromsiberia@gmail.com>
2022-10-04 20:40:06 +0500

    Alert label model done

---
+++ GitComunity/GCTitleLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GCTitleLabel.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 04.10.2022.
+//
+
+import UIKit
+
+class GCTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been imlemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+
