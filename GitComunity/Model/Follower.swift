7ad5f330c7fad2539c4ee0d3e251d0f79d7f7902
Maxim <swiftdevfromsiberia@gmail.com>
2022-10-06 22:01:27 +0500

    Model for JSON are created

---
+++ GitComunity/Model/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 06.10.2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    var 
+    
+}
---
+++ GitComunity/Model/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 06.10.2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avaterUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+    
+    
+    
+}
