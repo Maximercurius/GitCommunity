b9876b0f2d35b34baaaa217a0f15887bb196894b
Maxim <swiftdevfromsiberia@gmail.com>
2023-04-09 22:31:32 +0500

    Loading view with spinner added

--- GitComunity/Extensions/UIViewController+Ext.swift
+++ GitComunity/Extensions/UIViewController+Ext.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+fileprivate var containerView = UIView()
+
+
 
 extension UIViewController {
     
@@ -16,6 +19,33 @@ extension UIViewController {
             alertVC.modalPresentationStyle  = .overFullScreen
             alertVC.modalTransitionStyle    = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha  = 0.8
+        }
+        let activityindicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityindicator)
+        
+        activityindicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityindicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityindicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        activityindicator.startAnimating()
+    }
+    func dismissLoadingView() {
+        DispatchQueue.main.async{
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
