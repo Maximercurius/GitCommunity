ef96b29e8905c21e719df0f4e05a79065dc8e9ac
Maxim <swiftdevfromsiberia@gmail.com>
2023-04-21 23:51:02 +0500

    User detail info view is created

---
+++ GitComunity/CustomViews/GCSecondaryTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GCSecondaryTitleLabel.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 21.04.2023.
+//
+
+import UIKit
+
+class GCSecondaryTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been imlemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = .systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+
+}
