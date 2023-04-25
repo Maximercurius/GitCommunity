c2b823ca9af7cae6a0eb664f9eb21f42150573e5
Maxim <swiftdevfromsiberia@gmail.com>
2023-04-25 22:55:08 +0500

    Saving to favorites func is created

--- GitComunity/Screens/FavoritesListVC.swift
+++ GitComunity/Screens/FavoritesListVC.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
-
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+            
+        }
+        
     }
-
 }
