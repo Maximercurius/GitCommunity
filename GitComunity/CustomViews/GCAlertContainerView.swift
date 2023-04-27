646a3a04c03a0091bf04f29dd94de17a853f259d
Maxim <swiftdevfromsiberia@gmail.com>
2023-04-27 08:30:17 +0500

    Alert view refactored

---
+++ GitComunity/CustomViews/GCAlertContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  GCAlertContainerView.swift
+//  GitComunity
+//
+//  Created by Makarov_Maxim on 27.04.2023.
+//
+
+import UIKit
+
+class GCAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
