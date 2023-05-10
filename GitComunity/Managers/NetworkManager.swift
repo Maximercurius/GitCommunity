62715670af823ceb410f8df9e0180f92236c4417
Maxim <swiftdevfromsiberia@gmail.com>
2023-05-10 22:52:07 +0500

    downloading image are refactored

--- GitComunity/Managers/NetworkManager.swift
+++ GitComunity/Managers/NetworkManager.swift
@@ -97,35 +97,21 @@ class NetworkManager {
         }
     }
     
-    func downloadImage(from urlString: String, completed: @escaping(UIImage?) -> Void) {
+    func downloadImage(from urlString: String) async -> UIImage? {
         
         let cacheKey  = NSString(string: urlString)
-        if let image = cache.object(forKey: cacheKey) {
-            completed(image)
-            return
-        }
+        if let image = cache.object(forKey: cacheKey) { return image }
         
-        guard let url = URL(string: urlString) else {
-            completed (nil)
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+        guard let url = URL(string: urlString) else { return nil }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            guard let image = UIImage(data: data) else { return nil }
+            cache.setObject(image, forKey: cacheKey)
+            return image 
+        } catch {
+            return nil
             
-            guard let self = self,
-            error == nil,
-            let response  = response  as? HTTPURLResponse, response.statusCode == 200,
-            let data = data,
-                  let image = UIImage(data: data) else {
-                completed (nil)
-                return
-            }
-
-            self.cache.setObject(image, forKey: cacheKey)
-            
-            completed (image)
         }
-        task.resume()
     }
 }
 
